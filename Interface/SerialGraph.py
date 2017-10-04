42b900030fd93958cad2dedb51b50217245ba6ba
Alisson Claudino <alisson.claudino@ufrgs.br>
2017-10-04 12:23:34 -0300

    ADD: Comentários

--- Interface/SerialGraph.py
+++ Interface/SerialGraph.py
@@ -1,47 +1,55 @@
-from pyqtgraph.Qt import QtGui, QtCore
+from pyqtgraph.Qt import QtGui, QtCore       #importação de classes
 import pyqtgraph as pg
-import time
-import numpy as np
 import serial
+import time
 
-ser=serial.Serial('/dev/ttyUSB0',115200)
+ser = serial.Serial('/dev/ttyUSB0', 115200)  #Inicialização da Serial com baudrate 115200bps
+ser.setDTR(False)                            #Reset do arduino para reinicio da leitura
+time.sleep(0.022)
+ser.setDTR(True)
+
+ser.readline()                              #Primeiro valor da serial sai com lixo
+
 app = QtGui.QApplication([])
 win = pg.GraphicsWindow()
 p1 = win.addPlot()
 curve1 = p1.plot()
 
-y1=[]
-x1=[]
-indx = 0
+y1 = []                                     #Inicialização da lista dinâmica dos valores de y
+x1 = []                                     #Inicialização da lista dinâmica dos valores de x
+
+x_max = 50                                  #Variável que define o tamanho do eixo
+indx = 0                                    #Variável auxiliar de contagem
 
 
-def update():
-    global curve1, indx, y1,x1
-    while (ser.inWaiting() == 0):
+
+def update():                               #Método para atualizar os arrays com valores de x, y e atualizar o gráfico
+    global curve1, indx, y1, x1, ser        #Dá acesso das variáveis globais à função
+    while (ser.inWaiting() == 0):           #Aguarda a serial liberar dado
         pass
-    readData= float(ser.readline())
-    y1.append(readData)
-    print(readData)
-    indx+=1
-    if len(y1)>99:
-       del y1[0]
-       indx=100
+    readData = float(ser.readline())        #Lê o dado da serial
+    y1.append(readData)                     #Armazena o dado na lista do eixo y
+    indx += 1                               #Adiciona um na variavel auxiliar
+    if len(y1) > x_max-1:                   #Verifica se foi excedido o tamanho máximo do eixo x para o array
+        del y1[0]                           #Caso sim, libera o valor da primeira posição para manter o array, e consequentemente o gráfico, com escala constante
+        indx = x_max                        #impede indx de divergir
     else:
-        x1.append(indx)
+        x1.append(indx)                     #Vai adicionando valores de 0 a x_max no eixo x até que a lista x lote e tenha mesmo tamanho da lista y
 
-    curve1.setData(x1,y1)
-    p1.setXRange(0, 100, padding=0)
-    p1.setYRange(0,1023,padding=0)
-    app.processEvents()
+    curve1.setData(x1, y1)                  #Atualiza os dados da curva
+    p1.setYRange(0, 1023, padding=0)        #Define os limites do gráfico
+    p1.setXRange(0, max_x, padding=0)
 
-timer = QtCore.QTimer()
-timer.timeout.connect(update)
-timer.start(0)
+    app.processEvents()                     #
+
+timer = QtCore.QTimer()                     #
+timer.timeout.connect(update)               #
+timer.start(0)                              #
 
 
-
-
-if __name__ == '__main__':
+if __name__ == '__main__':                  #
     import sys
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_'):
-        QtGui.QApplication.instance().exec_()
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_'):   #
+        QtGui.QApplication.instance().exec_()                          #
+
+
