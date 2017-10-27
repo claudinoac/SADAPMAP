b01ee3cd5442419f91915043641d63aa4d26d400
Alisson Claudino <alisson.claudino@ufrgs.br>
2017-10-27 17:36:19 -0200

    Adição de mais 4 sensores, dois ADCs, 8 canais em simultâneo

--- Firmware/ADS1115.ino/ADS1115.ino.ino
+++ Firmware/ADS1115.ino/ADS1115.ino.ino
@@ -1,9 +1,10 @@
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
-Adafruit_ADS1115 ads;  
-int16_t adc0, adc1, adc2, adc3;
-  float val0;
+Adafruit_ADS1115 ads0(0x48);
+Adafruit_ADS1115 ads1(0x49);
+  
+int16_t adc0, adc1, adc2, adc3, adc4, adc5, adc6, adc7;
 void setup(void) 
 {
   Serial.begin(115200);
@@ -12,16 +13,20 @@ void setup(void)
   //Serial.println("Getting single-ended readings from AIN0..3");
   //Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
   
-  ads.setGain(GAIN_TWOTHIRDS);        // ganho 1x   +/- 4.096V  1 bit = 2mV      0.125mV
-  ads.begin();
+  ads0.begin();
+  ads1.begin();
 }
 
 void loop(void) 
 {
-  adc0 = ads.readADC_SingleEnded(0);
-  adc1 = ads.readADC_SingleEnded(1);
-  adc2 = ads.readADC_SingleEnded(2);
-  adc3 = ads.readADC_SingleEnded(3);;
+  adc0 = ads0.readADC_SingleEnded(0);
+  adc1 = ads0.readADC_SingleEnded(1);
+  adc2 = ads0.readADC_SingleEnded(2);
+  adc3 = ads0.readADC_SingleEnded(3);
+  adc4 = ads1.readADC_SingleEnded(0);
+  adc5 = ads1.readADC_SingleEnded(1);
+  adc6 = ads1.readADC_SingleEnded(2);
+  adc7 = ads1.readADC_SingleEnded(3);
   Serial.print(adc0);
   Serial.print(" ");
   Serial.print(adc1);
@@ -29,8 +34,18 @@ void loop(void)
   Serial.print(adc2);
   Serial.print(" ");
   Serial.print(adc3);
+  Serial.print(" ");
+  Serial.print(adc4);
+  Serial.print(" ");
+  Serial.print(adc5);
+  Serial.print(" ");
+  Serial.print(adc6);
+  Serial.print(" ");
+  Serial.print(adc7);
+  Serial.print(" ");
+  Serial.print(millis());
   Serial.print("\n");
-  while(millis()%20!=0)
-  {}
+  while(millis()%100!=0);
+ 
   
 }
--- Interface/SerialGraph.py
+++ Interface/SerialGraph.py
@@ -4,7 +4,7 @@ import serial
 import time
 from mainwindow import Ui_MainWindow
 
-ser = serial.Serial('/dev/ttyUSB1', 115200)  #Inicialização da Serial com baudrate 115200bps
+ser = serial.Serial('/dev/ttyUSB0', 115200)  #Inicialização da Serial com baudrate 115200bps
 ser.setDTR(False)                            #Reset do arduino para reinicio da leitura
 time.sleep(0.022)
 ser.setDTR(True)
@@ -29,13 +29,21 @@ curve1 = p1.plot(pen='r',name="Sensor 1")
 curve2 = p1.plot(pen='g',name="Sensor 2")
 curve3 = p1.plot(pen='w',name="Sensor 3")
 curve4 = p1.plot(pen='y',name="Sensor 4")
-curve1.name="Sensor 1"
+curve5 = p1.plot(pen='b',name="Sensor 5")
+curve6 = p1.plot(pen='m',name="Sensor 6")
+curve7 = p1.plot(pen='c',name="Sensor 7")
+curve8 = p1.plot(name="Sensor 8")
+
 window.CentralGraph.setFixedSize(690,550)
 layout.setPreferredSize(640,500)
 y1 = []                                     #Inicialização da lista dinâmica dos valores de y
 y2 = []
 y3 = []
 y4 = []
+y5 = []
+y6 = []
+y7 = []
+y8 = []
 x1 = []                                     #Inicialização da lista dinâmica dos valores de x
 x_max = 200                                  #Variável que define o tamanho do eixo
 indx = 0                                    #Variável auxiliar de contagem
@@ -56,12 +64,16 @@ def update():                               #Método para atualizar os arrays co
         pass
     readData = ser.readline()       #Lê o dado da serial
     readData=readData.decode('utf8')
-    dado=readData.split(' ',3)
-    print(dado[0])
+    dado=readData.split(' ',8)
+    print(dado[3])
     y1.append(float(dado[0])*5/32767)                     #Armazena o dado na lista do eixo y
     y2.append(float(dado[1])*5/32767)
     y3.append(float(dado[2])*5/32767)
     y4.append(float(dado[3])*5/32767)
+    y5.append(float(dado[4])*5/32767)
+    y6.append(float(dado[5])*5/32767)
+    y7.append(float(dado[6])*5/32767)
+    y8.append(float(dado[7])*5/32767)
 
     indx += 1                               #Adiciona um na variavel auxiliar
     if len(y1) > x_max-1:                   #Verifica se foi excedido o tamanho máximo do eixo x para o array
@@ -69,6 +81,10 @@ def update():                               #Método para atualizar os arrays co
         del y2[0]
         del y3[0]
         del y4[0]
+        del y5[0]
+        del y6[0]
+        del y7[0]
+        del y8[0]
         indx = x_max                        #impede indx de divergir
     else:
         x1.append(indx)                     #Vai adicionando valores de 0 a x_max no eixo x até que a lista x lote e tenha mesmo tamanho da lista y
@@ -77,6 +93,10 @@ def update():                               #Método para atualizar os arrays co
     curve2.setData(x1,y2)
     curve3.setData(x1,y3)
     curve4.setData(x1,y4)
+    curve5.setData(x1, y5)
+    curve6.setData(x1, y6)
+    curve7.setData(x1, y7)
+    curve8.setData(x1, y8)
 
 
     #app.processEvents()                     #
