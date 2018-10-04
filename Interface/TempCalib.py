248c5d5f8a4ecf1c2b31c6dce8ddbd083658ac7d
Alisson Claudino <acjesus@inf.ufrgs.br>
2018-10-04 20:34:40 -0300

    ADD: Integration between InterfaceTimer and TempCalib classes

--- Interface/TempCalib.py
+++ Interface/TempCalib.py
@@ -11,6 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from InterfaceTimer import InterfaceTimer
 
 class TempCalib(InterfaceTimer):
+
     def setupUi(self, MainWindow):
         self.MainWindow = MainWindow
         MainWindow.setObjectName("MainWindow")
